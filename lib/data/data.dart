5915566b7270018289db3a1d56c34aa52d56cb7c
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-05-27 23:55:59 +0530

    Changes Committed on 2024-05-27, 23:55

---
+++ lib/data/data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+var myData = [
+  {
+    'icon': Icons.food_bank,
+    'color': Colors.yellow[600],
+    'name': 'Food',
+    'totalAmount': '45.00',
+    'date': 'Today',
+  }
+];
